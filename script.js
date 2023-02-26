63190b048981a9559402ca8b534e9c24e661b3a4
Andbaik <andbaik@mail.ru>
2023-02-26 12:02:02 +0300

    solve

--- script.js
+++ script.js
@@ -77,7 +77,7 @@ console.log(worker.getSalary());
 // Создайте объект city1 (var city1 = {}), укажите у него свойства name 
 // (название города, строка) со значением «ГородN» и population 
 // (населенность города, число) со значением 10 млн.
-/*
+
 var city1 = {};
     city1.name = 'ГородN';
     city1.population = 1000000;
@@ -85,10 +85,10 @@ var city1 = {};
     city1.exportStr = function(){return `Город ${this.name}  популяция ${this.population}`};
 
 console.log(city1);
-*/
+
 
 // Создайте объект city2 через нотацию {name: "ГородM", population: 1e6}.
-/*
+
 var city2 = {
     name: "ГородM",
     population: 1e6,
@@ -101,7 +101,7 @@ var city2 = {
     };
 
 console.log(city2);
-*/
+
 // Создайте у объектов city1 и city2 методы getName(), которые вернут 
 // соответствующие названия городов.
 /*
@@ -114,19 +114,25 @@ console.log(city2.getName());
 // «name=ГородN\npopulation=10000000\n». Для второго города будет строка 
 // со своими значениями. Примечание: можно обращаться к каждому свойству 
 // через цикл for/in, но методы объекта возвращать не нужно.
-/*
+
 console.log(city1.exportStr());
 console.log(city2.exportStr());
 
-for (let i=1;i<3;i++){
-    console.log(city{i}.name);
-}*/
+function exportStr() {return `name=${this.name}\npopulation=${this.population}\n`;}
+city1.exportStr = exportStr;
+city2.exportStr = exportStr;
 
 // Создайте глобальную функцию getObj(), которая возвращает this. 
 // А у каждого из объектов city1 или city2 метод getCity, который 
 // ссылается на getObj. Проверьте работу метода. Примечание: к объекту 
 // вызова можно обратиться через this..
 
+function getObj () { return this }
+
+city1 = {name: 'ГородN', population: '1млн', getCity: getObj};
+city2 = {name: 'ГородM', population: '2млн', getCity: getObj};
+
+console.log (city1.getCity().name + ' ' + city2.getCity().population);
 
 // Создайте массив d1 с числовыми величинами 45,78,10,3.  
 // Добавьте в массив d1 еще одно число ( d1[7] = 100). 
@@ -140,17 +146,34 @@ console.log (d1[6] , d1[7]);
 
 // Создайте массив d2 с числовыми величинами 45,78,10,3. 
 // Посчитайте в переменную sum2 сумму чисел в нем, при помощи цикла for.
-let d2 = [45,78,10,3];
-let sum2;
+/* var d2 = [45,78,10,3];
+var sum2 = 0;
 for (let j=0; j < d2.length; j++){
     sum2 += d2 [j];
 }
 console.log (sum2);
+*/
 
 // Создайте массив d3 с числовыми величинами 45,78,10,3. 
 // Добавьте в массив d3 еще одно число (например, d[7] = 100) 
 // Посчитайте в переменную sum3 сумму чисел в нем, при помощи цикла for/in.
-
+/*
+var d3 = [45,78,10,3];
+d3[7] = 100;
+console.log (d3);
+var sum4 = 0;
+for (var el in d3){
+    sum4+=d3[el];
+}
+console.log(sum4);
+*/
 // Создайте массив d4 с числовыми величинами 45,78,10,3. 
 // Напишите функцию сортировки my(a,b), которая при вызове d4.sort(my) 
 // отсортирует элементы массива по убыванию чисел. Вызовите d4.sort(my)
+/*
+var d4 = [45,78,10,3];
+d4.sort(function(a, b) {
+    return a - b;
+  });
+  console.log(d4);
+  */
